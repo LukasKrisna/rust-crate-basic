966467fb1208ba257648c996738fd111bd1fa965
LukasKrisna <krisnaprasriyan@gmail.com>
2025-10-06 01:24:18 +0700

    init

---
+++ say-hello/target/debug/deps/say_hello-1291dd9ac5fe372d.d
@@ -0,0 +1,5 @@
+/Users/mac/Lukas/Smartelco SST/rust-crate/say-hello/target/debug/deps/say_hello-1291dd9ac5fe372d.d: src/lib.rs
+
+/Users/mac/Lukas/Smartelco SST/rust-crate/say-hello/target/debug/deps/libsay_hello-1291dd9ac5fe372d.rmeta: src/lib.rs
+
+src/lib.rs:
