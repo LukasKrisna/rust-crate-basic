136fe3e83d0f7f4e558ca1706655e81fd64e2308
LukasKrisna <krisnaprasriyan@gmail.com>
2025-10-06 01:38:08 +0700

    crates io publich

--- say-hello/src/lib.rs
+++ say-hello/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn say_hello(name: &str) -> String {
+    format!("Hello, {}!", name)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn say_goodbye(name: &str) -> String {
+    format!("Goodbye, {}!", name)
 }
---
+++ say-hello/target/debug/deps/say_hello-bf1c05e4fdd3317b.d
@@ -0,0 +1,5 @@
+/Users/mac/Lukas/Smartelco SST/rust-crate/say-hello/target/debug/deps/say_hello-bf1c05e4fdd3317b.d: src/lib.rs
+
+/Users/mac/Lukas/Smartelco SST/rust-crate/say-hello/target/debug/deps/say_hello-bf1c05e4fdd3317b: src/lib.rs
+
+src/lib.rs:
